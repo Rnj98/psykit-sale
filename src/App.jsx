c1925b24748e19f666e66dda9c04f9dfab37007e
Ryan Jolley <ryan.jolley0@gmail.com>
2025-04-30 18:43:19 -0600

    changed logo + site name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/MainPage/Home';
 import Navbar from './components/Navbar/Navbar';
 import DemoRequest from './components/DemoRequest/DemoRequest';
